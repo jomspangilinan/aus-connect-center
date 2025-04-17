6524e5897e9ab29860118a30169fc3ded57bcfa6
jomspangilinan <pangilinanjohnjomel@gmail.com>
2025-04-17 11:35:59 +0800

    Initial commit

---
+++ test/ausconnect.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ausconnect from '../lib/ausconnect-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ausconnect-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ausconnect.AusconnectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
